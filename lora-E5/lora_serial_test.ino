9df1ebb895fb56cf09e7c64a98177e39a7e560e8
dev-ansh-r <rdevanshshukla02@gmail.com>
2022-08-15 11:00:31 +0530

    added configured code

--- lora-E5/lora_serial_test.ino
+++ lora-E5/lora_serial_test.ino
@@ -1,23 +1,31 @@
 #include<SoftwareSerial.h>
 
 SoftwareSerial loraserial(2,3);
+char appeui = 8000000000000006;
+char deveui = 2CF7F1203230D4E7;
+char appkey = 9648BEF80D21DA31CA835206876FDAF9;
 
 void setup(){
     Serial.begin(9600);
     loraserial.begin(9600);
+// initialize the serial communications  and set up id's and key's
 
-    Serial.println("hello of arduino");
-
-    loraserial.println("hello of lora");
+    Serial.println("Process initiated");
+    loraserial.println("AT+ID=appeui," + appeui);
+    loraserial.println("AT+ID=deveui," + deveui);
+    loraserial.println("AT+ID=appkey," + appkey);
+    loraserial.println("AT+JOIN");
 }
 
 void loop(){
     if(Serial.available()){
-        char c = Serial.read();
+        char c = 10;
+        loraserial.println("AT+MSG="+ c);
         Serial.println(loraserial.write(c));
     }
     if(loraserial.available()){
         char c = loraserial.read();
+        Serial.write(c);
         Serial.println(c);
     }
 }
