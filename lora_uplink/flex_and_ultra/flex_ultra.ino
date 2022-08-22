7d20875821191dd488f4c18fc80f0cf7587ad1c0
dev-ansh-r <rdevanshshukla02@gmail.com>
2022-08-22 13:47:23 +0530

    Final code is upated

--- lora_uplink/flex_and_ultra/flex_ultra.ino
+++ lora_uplink/flex_and_ultra/flex_ultra.ino
@@ -8,10 +8,10 @@ const int flexPin = A0;
 // Pin connected to voltage divider output
 
 // Change these constants according to your project's design
-const float VCC = 5;      // voltage at Ardunio 5V line
-const float R_DIV = 10000.0;  // resistor used to create a voltage divider
-const float flatResistance = 16000.0; // resistance when flat
-const float bendResistance = 17500.0;  // resistance at 90 deg
+const int VCC = 5;      // voltage at Ardunio 5V line
+const int R_DIV = 10000;  // resistor used to create a voltage divider
+const int flatResistance = 16000; // resistance when flat
+const int bendResistance = 17500;  // resistance at 90 deg
 
 // declare ultrasonic sensor pins
 int trig = 10;
@@ -34,7 +34,7 @@ void setup() {
 }
 
 
-void loop(){ 
+void loop(){
 // set pin modes for ultrasonic sensor
     digitalWrite(trig, LOW);
     delayMicroseconds(2);
@@ -47,40 +47,50 @@ void loop(){
     delayMicroseconds(2);
     distance = duration * (0.034/2);
     str1 = String(distance, 3);
-    if(distance < 5){
-        Serial.println(str1);
-      
+   
+    if(distance >10){
+      loraserial.println("AT+MSG=" + str1);
+      delay(1000);
+     Serial.println(str1);
+     delay(1000);
    if(Serial.available()){
-       loraserial.println("AT+MSG=" +str1);
-       loraserial.println("AT+MSG=" +str2);    
-      
+       loraserial.println("AT+MSG=" +str1);      
+     
    }
     if(loraserial.available()){
         Serial.println(loraserial.readString());
         delay(50);
 
-    } 
+    }
+     
+     
   }
 
   // Use the calculated resistance to estimate the sensor's bend angle:
   int ADCflex = analogRead(flexPin);
-  float Vflex = ADCflex * VCC / 1023.0;
-  float Rflex = R_DIV * (VCC / Vflex - 1.0);
-  float angle = map(Rflex, flatResistance, bendResistance, 0, 90.0);
-  str2 = String(angle, 3);
-  Serial.println("Bend: " + String(angle) + " degrees");
-  Serial.println();
-  if(Serial.available()){
-       str2=String(angle);
+  int Vflex = ADCflex * VCC / 1023;
+  int Rflex = R_DIV * (VCC / Vflex - 1);
+  int  angle = map(Rflex, flatResistance, bendResistance, 0, 90);
+  //Serial.println("Bend: " + String(angle) + " degrees");
+  //Serial.println();
+  str2=String(angle);
+   
+
+  if(angle>=-2160){
+     loraserial.println("AT+MSG=" + str2);
+     delay(1000);
+     Serial.println(str2);
+     delay(1000);
+   if(Serial.available()){
        loraserial.println("AT+MSG=" + str2);      
-      
+     
    }
     if(loraserial.available()){
         Serial.println(loraserial.readString());
         delay(50);
 
-    } 
+    }
+  }
 
   delay(500);
 }
-
