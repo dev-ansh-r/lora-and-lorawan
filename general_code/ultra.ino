9b66914199451ba0e1e8e62d35362a7ad2ffacbe
Devansh Shukla R <59604988+dev-ansh-r@users.noreply.github.com>
2023-03-21 04:59:03 +0530

    Update ultra.ino

--- general_code/ultra.ino
+++ general_code/ultra.ino
@@ -1,11 +1,3 @@
-// ---------------------------------------------------------------- //
-// Arduino Ultrasoninc Sensor HC-SR04
-// Re-writed by Arbi Abdul Jabbaar
-// Using Arduino IDE 1.8.7
-// Using HC-SR04 Module
-// Tested on 17 September 2019
-// ---------------------------------------------------------------- //
-
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
 
